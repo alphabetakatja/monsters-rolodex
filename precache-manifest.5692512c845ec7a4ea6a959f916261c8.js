4c7a3281513380ed45b3187de5da4d6a4abdbc50
Katja Stefan <katja.stefan18@gmail.com>
2020-07-14 21:41:29 +0200

    Updates

---
+++ precache-manifest.5692512c845ec7a4ea6a959f916261c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817c2dee7bd11b4e6a43e3dd2d9fcbed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f73909d635db543a48c3",
+    "url": "/monsters-rolodex/static/css/main.d6370b44.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73909d635db543a48c3",
+    "url": "/monsters-rolodex/static/js/main.6a2275b0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
